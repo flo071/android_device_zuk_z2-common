5bdda08f15c3a33c0bef4eea56ad97a19cf1f2e8
flo071 <flo.hintermeier@gmail.com>
2017-01-06 12:48:41 +0100

    update for LineageOS

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +23,7 @@ $(call inherit-product, device/zuk/z2/device.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_z2
+PRODUCT_NAME := lineage_z2
 PRODUCT_DEVICE := z2
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
